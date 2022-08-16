ad2d7d4c0032ada887fa742d4e6b9301840d8954
Phoenicck <2962548150@qq.com>
2022-08-16 20:30:19 +0800

    v0.4

--- project1/include/checkCard.h
+++ project1/include/checkCard.h
@@ -1,14 +1,18 @@
 #pragma once
-
 #include<opencv2/opencv.hpp>
+#include<opencv2/highgui.hpp>
+#include<opencv2/imgproc.hpp>
 #include<vector>
 using namespace cv;
-
+using namespace std;
 class Check {//用来检测银行卡图片的类
 public:
 	Mat erzhihua(Mat& image);//二值化
-	void findmubiao(Mat& image);
-	void moban(Mat& image);
+	void findcardnum(Mat& image, vector<Mat>&cardNumber , vector<Rect>&Re);
+	void findmoban(Mat& image, vector<Mat>&mubiao);
 	void tezheng(Mat& img1, Mat& img2);
+	vector<int> acgHash(Mat& image);
+	int xiangsidu(vector<int> &has1, vector<int>&has2);
+	vector< vector<int>> mobanHash(vector<Mat>&mubiao);
 
 };
--- project1/main.cpp
+++ project1/main.cpp
@@ -2,19 +2,52 @@
 #include<opencv2/opencv.hpp>
 #include<checkCard.h>
 #include<vector>
+#include<algorithm>
 
 using namespace std;
 using namespace cv;
 int main() {
 	Mat m1 = imread("src/ocr_a_reference.png");
 	Mat m2 = imread("src/credit_card_01.png");
+	Mat dst;
+	vector<Mat>mubiao(10);
+	vector<Mat> cardNumber;
 
+	vector<int>hash1;
+	vector<int>hash2;
+	vector< vector<int>>mbHash;
 	Check demo;
 	//demo.showContours(m1);
 	//demo.findmubiao(m2);
-	demo.moban(m1);
-	//demo.tezheng(m1, m2);
+	dst = ~m1;
+	demo.findmoban(dst,mubiao);
+	mbHash=demo.mobanHash(mubiao);
+	vector<Rect>Re;
+	demo.findcardnum(m2,cardNumber,Re);
 
+
+	for (int i = 0; i < cardNumber.size(); i++) {
+		vector<int> arr(10);
+		vector<int>arr1 = demo.acgHash(cardNumber[i]);
+		for (int j = 0; j < 10; j++) {
+			arr[j] = demo.xiangsidu(mbHash[j], arr1);
+		}
+		int maxPosition = max_element(arr.begin(), arr.end()) - arr.begin();
+		/*int x = cardNumber[i].tl().x;
+		int y = cardNumber[i].tl().y;*/
+		
+		cout << "最相似的是：" << maxPosition << endl;
+		int x = Re[i].tl().x;
+		int y = Re[i].tl().y;
+		string str = to_string(maxPosition);
+		putText(m2, str, Point(x, y), FONT_HERSHEY_COMPLEX, 0.8, Scalar(0, 255, 0));
+	}
+	//hash1 = demo.acgHash(cardNumber[5]);
+	//hash2 = demo.acgHash(mubiao[7]);
+	//int res = demo.xiangsidu(hash1, hash2);
+	//demo.tezheng(mubiao[7], cardNumber[5]);
+	//cout << "相似度为： " << res << endl;
+	imshow("result", m2);
 	waitKey(0);
 	return 0;
 }
