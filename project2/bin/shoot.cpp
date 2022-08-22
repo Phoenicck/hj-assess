7917a8ea09a2fb56d2d953e6e2dfceed90059f82
Phoenicck <2962548150@qq.com>
2022-08-22 10:28:08 +0800

    p2-v0.6

--- project2/bin/shoot.cpp
+++ project2/bin/shoot.cpp
@@ -81,43 +81,129 @@ Mat CheckJiaban::chuli(Mat& image) {
 	//imshow("thre", imgDil);
 	return imgDil;
 }
-
+//11111111111111111111111111111111111111111111111111
 //找到灯条，并将其画出  ，返回灯条的最小矩形
+//vector<RotatedRect> CheckJiaban::findLight(Mat& image, Mat& src) {
+//	vector<vector<Point>> contours;//轮廓的点集
+//	findContours(image, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);//将轮廓找出
+//	//drawContours(src, contours, -1, Scalar(0, 255, 0));
+//	vector<vector<Point>> conploy(contours.size());//拟合点集
+//	vector<RotatedRect> minrec(contours.size());//最小矩形的点集
+//	for (int i = 0; i < contours.size(); i++) {
+//		float peri = arcLength(contours[i], true);
+//		approxPolyDP(contours[i], conploy[i], peri * 0.02, true);//拟合
+//		Rect re = boundingRect(contours[i]);
+//		double w = re.width;
+//		double h = re.height;
+//		double bili = h / w;
+//		if (bili > 2.5) {//因为灯条长宽比最少也是2.54
+//			minrec.push_back(minAreaRect(contours[i]));//将最小矩形传入
+//		}
+//	}
+//	//rectangle(src, Re[0], Scalar(0, 0, 255));
+//	//vector<Rect>re(Re.size());
+//	vector<RotatedRect> Minrec;//满足条件的最小矩形
+//	for (int i = 0; i < minrec.size(); i++) {
+//		Size2f tempwh = minrec[i].size;
+//		double w = tempwh.width;
+//		double h = tempwh.height;
+//		double bili1 = h / w;//这是应为它的长宽提取是有顺序，所以长宽比与宽长比是有共通性的
+//		double bili2 = w / h;
+//		if (bili1 > 2.5 || bili2 > 2.5) {//
+//			//Drawrotedrec(src, minrec[i], Scalar(0, 0, 255), 1);
+//			//(src, minrec[i], Scalar(0, 0, 255));
+//			Minrec.push_back(minrec[i]);//满足条件的矩形放入
+//		}
+//	}
+//	//imshow("dad", src);//调试用的
+//	return Minrec;
+//}
+
+//将甲板的矩形找出
+//RotatedRect CheckJiaban::findJiban(vector<RotatedRect>& light, Mat& src) {
+//	Mat result = src.clone();
+//	if (light.size() == 0) {
+//		cout << "无法识别甲板" << endl;
+//	}
+//	//
+//	vector<vec2RotatedRect>mubiao;//比较列表
+//	for (int i = 0; i < light.size(); i++) {//一个从头找，另外一个反方向找
+//		for (int j = light.size() - 1; j > i; j--) {
+//			float temp = light[i].angle / light[j].angle;
+//			if (temp > 0.9 && temp < 1.1) {//比较矩形的旋转角度，精度只能试着来
+//				vec2RotatedRect	pipei(light[i], light[j]);
+//				mubiao.push_back(pipei);
+//			}
+//		}
+//	}
+//
+//	vector<double>area;
+//	vector<vector<Point2i>> heti(mubiao.size());
+//	vector<RotatedRect>roatedrects(mubiao.size());
+//	for (int i = 0; i < mubiao.size(); i++) {
+//
+//
+//		Point2f ps1[4];//将8个点合体
+//
+//		//先是4个
+//		mubiao[i].arr[0].points(ps1);
+//		for (int j = 0; j < 4; j++) {
+//			heti[i].push_back(ps1[j]);
+//		}
+//		Point2f ps2[4];
+//		mubiao[i].arr[1].points(ps2);
+//		for (int j = 0; j < 4; j++) {
+//			heti[i].push_back(ps2[j]);
+//		}
+//
+//		roatedrects[i] = minAreaRect(heti[i]);//对已经匹配成功的灯条画出他们的矩形
+//		Drawrotedrec(src, roatedrects[i], Scalar(0, 255, 0), 2);//画出已检测的甲板
+//		double area_temp = roatedrects[i].size.width * roatedrects[i].size.height;
+//		area.push_back(area_temp);//因为把面积作为比较的因素，把面积储存
+//	}
+//	int maxPosition = max_element(area.begin(), area.end()) - area.begin();
+//	//面积最大的装甲板被检测到
+//	Drawrotedrec(src, roatedrects[maxPosition], Scalar(0, 0, 255), 2);//画出目标矩形
+//	//imshow("allJiaban", src);//这两个显示为了将不同的区分开来
+//	//imshow("result", result);
+//	return roatedrects[maxPosition];
+//};
+
+
+
+
+
+
+//2222222222222222222222222222222222222222222
 vector<RotatedRect> CheckJiaban::findLight(Mat& image, Mat& src) {
 	vector<vector<Point>> contours;//轮廓的点集
 	findContours(image, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);//将轮廓找出
 	//drawContours(src, contours, -1, Scalar(0, 255, 0));
-	vector<vector<Point>> conploy(contours.size());//拟合点集
-	vector<RotatedRect> minrec(contours.size());//最小矩形的点集
+	//vector<vector<Point>> conploy(contours.size());//拟合点集
+	vector<RotatedRect> minrec;//最小矩形的点集
 	for (int i = 0; i < contours.size(); i++) {
-		float peri = arcLength(contours[i], true);
-		approxPolyDP(contours[i], conploy[i], peri * 0.02, true);//拟合
-		Rect re = boundingRect(contours[i]);
-		double w = re.width;
-		double h = re.height;
-		double bili = h / w;
-		if (bili > 2.5) {//因为灯条长宽比最少也是2.54
+		if (contours.size() < 1) break;
+		float light_area = contourArea(contours[i]);//面积筛选
+		if (light_area < 100) continue;
+		if (contours[i].size() < 6) continue;//满足椭圆拟合矩形
+		RotatedRect light_minrec = fitEllipse(contours[i]);
+		double a = light_minrec.size.width;
+		double b = light_minrec.size.height;
+		// double w = max(a, b);
+		// double h = min(a, b);
+		 double wh_bili = b/a;
+		 if (wh_bili < 3) continue;
 			minrec.push_back(minAreaRect(contours[i]));//将最小矩形传入
-		}
+			//Drawrotedrec(src, light_minrec, Scalar(255, 0, 255), 1);
+		
 	}
-	//rectangle(src, Re[0], Scalar(0, 0, 255));
-	//vector<Rect>re(Re.size());
-	vector<RotatedRect> Minrec;//满足条件的最小矩形
-	for (int i = 0; i < minrec.size(); i++) {
-		Size2f tempwh = minrec[i].size;
-		double w = tempwh.width;
-		double h = tempwh.height;
-		double bili1 = h / w;//这是应为它的长宽提取是有顺序，所以长宽比与宽长比是有共通性的
-		double bili2 = w / h;
-		if (bili1 > 2.5 || bili2 > 2.5) {//
-			//Drawrotedrec(src, minrec[i], Scalar(0, 0, 255), 1);
-			//(src, minrec[i], Scalar(0, 0, 255));
-			Minrec.push_back(minrec[i]);//满足条件的矩形放入
-		}
-	}
+	
 	//imshow("dad", src);//调试用的
-	return Minrec;
+	//waitKey(0);
+	return minrec;
 }
+
+
 
 //将甲板的矩形找出
 RotatedRect CheckJiaban::findJiban(vector<RotatedRect>& light, Mat& src) {
@@ -129,14 +215,19 @@ RotatedRect CheckJiaban::findJiban(vector<RotatedRect>& light, Mat& src) {
 	vector<vec2RotatedRect>mubiao;//比较列表
 	for (int i = 0; i < light.size(); i++) {//一个从头找，另外一个反方向找
 		for (int j = light.size() - 1; j > i; j--) {
-			float temp = light[i].angle / light[j].angle;
-			if (temp > 0.9 && temp < 1.1) {//比较矩形的旋转角度，精度只能试着来
-				vec2RotatedRect	pipei(light[i], light[j]);
-				mubiao.push_back(pipei);
-			}
+			float angle_gap =abs( light[i].angle - light[j].angle);
+			float y_gap = abs(light[i].center.y - light[j].center.y);
+			if (angle_gap < 5 || angle_gap>85) {
+				if(y_gap<50){
+					vec2RotatedRect	pipei(light[i], light[j]);
+					mubiao.push_back(pipei);
+					Drawrotedrec(src, light[i], Scalar(255, 0, 255), 1);
+				}
+			}	
 		}
 	}
-
+	imshow("dad", src);
+	//waitKey(0);
 	vector<double>area;
 	vector<vector<Point2i>> heti(mubiao.size());
 	vector<RotatedRect>roatedrects(mubiao.size());
@@ -157,9 +248,13 @@ RotatedRect CheckJiaban::findJiban(vector<RotatedRect>& light, Mat& src) {
 		}
 
 		roatedrects[i] = minAreaRect(heti[i]);//对已经匹配成功的灯条画出他们的矩形
-		Drawrotedrec(src, roatedrects[i], Scalar(0, 255, 0), 2);//画出已检测的甲板
-		double area_temp = roatedrects[i].size.width * roatedrects[i].size.height;
-		area.push_back(area_temp);//因为把面积作为比较的因素，把面积储存
+		float bili1 = roatedrects[i].size.width/ roatedrects[i].size.height;
+		//float bili2 = roatedrects[i].size.height / roatedrects[i].size.width;
+		if (bili1 < 3 ) {
+			Drawrotedrec(src, roatedrects[i], Scalar(0, 255, 0), 2);//画出已检测的甲板
+			double area_temp = roatedrects[i].size.width * roatedrects[i].size.height;
+			area.push_back(area_temp);//因为把面积作为比较的因素，把面积储存
+		}
 	}
 	int maxPosition = max_element(area.begin(), area.end()) - area.begin();
 	//面积最大的装甲板被检测到
@@ -168,13 +263,17 @@ RotatedRect CheckJiaban::findJiban(vector<RotatedRect>& light, Mat& src) {
 	//imshow("result", result);
 	return roatedrects[maxPosition];
 };
-void CheckJiaban::zuobiao(RotatedRect& jiaban,Mat&src) {//应该选择已经匹配成功的一个灯条作为相机坐标转为世界坐标的模板，要不然，可能不是灯条
+
+//22222222222222222222222222222222222222222222222222
+
+
+void CheckJiaban::zuobiao(RotatedRect& jiaban, Mat& src) {//应该选择已经匹配成功的一个灯条作为相机坐标转为世界坐标的模板，要不然，可能不是灯条
 	//主要是甲板的四个点作为检测，这样就不用角点检测，
 	//用甲板的尺寸作为转为世界坐标的依据
 	Point2f ps[4];
 	vector<Point2f>Ps;//这个为了pnp测距的时候用的，跟ps一样的
 	jiaban.points(ps);//将四个点取下
-	Point2f pmin=ps[0];//找到最高的点
+	Point2f pmin = ps[0];//找到最高的点
 	float maxDistance = 0;
 	//找到最高的点，和最长的边
 	for (int i = 0; i < 4; i++) {
@@ -196,28 +295,28 @@ void CheckJiaban::zuobiao(RotatedRect& jiaban,Mat&src) {//应该选择已经匹配成功的
 
 		}
 	}
-	
+
 	//cout <<"max is: " << pmin << endl;//仅仅为了显示选择的点是否正确
 	Point2f newPs[4];//找到世界坐标
-	float biliChi =14/maxDistance;//像素距离与实际的比例尺
+	float biliChi = 14 / maxDistance;//像素距离与实际的比例尺
 	for (int i = 0; i < 4; i++) {
-		newPs[i] = (ps[i] - pmin)*biliChi;//将最高的点作为原点，之后再按比例转换
+		newPs[i] = (ps[i] - pmin) * biliChi;//将最高的点作为原点，之后再按比例转换
 		//cout << newPs[i] << endl;//仅仅为了显示选择的点是否正确
 	}
-	
+
 	//cout <<"调整后" << distance(newPs[0], newPs[1]) << endl;仅为了显示
 	//cout<<"调整后"<< distance(newPs[2], newPs[1]) << endl;
 
 	vector<Point3f> objP;//将世界坐标放进数组
 	for (int i = 0; i < 4; i++) {
-		Point3f temp(newPs[i].x,newPs[i].y, 0);
+		Point3f temp(newPs[i].x, newPs[i].y, 0);
 		objP.push_back(temp);
 		//cout <<"shijie: " << objP[i] << endl;
 	}
 	//这部分是求解pnp
 	Mat rvecs = Mat::zeros(3, 1, CV_64FC1);//旋转向量
 	Mat tvecs = Mat::zeros(3, 1, CV_64FC1);//平移向量
-	solvePnP(objP,Ps,CameraMatrix,JibianMatrix,rvecs,tvecs);//求解pnp
+	solvePnP(objP, Ps, CameraMatrix, JibianMatrix, rvecs, tvecs);//求解pnp
 	Mat rotM = Mat::eye(3, 3, CV_64F);//旋转矩阵
 	Mat rotT = Mat::eye(3, 3, CV_64F);//平移矩阵
 
@@ -229,7 +328,7 @@ void CheckJiaban::zuobiao(RotatedRect& jiaban,Mat&src) {//应该选择已经匹配成功的
 	P = (rotM.t()) * tvecs;
 	//将深度输出
 	cout << P << endl;
-	cout <<"diastance is:" << P.at<double>(2, 0) << "cm" << endl;
+	cout << "diastance is:" << P.at<double>(2, 0) << "cm" << endl;
 	string str = to_string(P.at<double>(2, 0)) + "cm";
 	putText(src, str, jiaban.center, FONT_HERSHEY_COMPLEX, 0.5, Scalar(0, 255, 255));
 }
--- project2/main.cpp
+++ project2/main.cpp
@@ -7,15 +7,33 @@ using namespace cv;
 
 //目前先拿7号作为测试，也是现在比较好的一个测试结果
 int main() {
-	Mat src = imread("src/high/7.jpg");
-	CheckJiaban demo;
-	Mat jiaozheng = demo.jiaozheng(src);
-	Mat chuli= demo.chuli(jiaozheng);
-	vector<RotatedRect>light= demo.findLight(chuli,src);
-	RotatedRect jiaban = demo.findJiban(light,src);
-	demo.zuobiao(jiaban,src);//选择已经匹配的灯管这样准确性很高
-	imshow("result", src);
-	imwrite("output/7.jpg", src);
-	waitKey(0);
+
+	for (int i = 126; i <= 147; i++) {
+		string path = "src/low/" + to_string(i) + ".jpg";
+		Mat src = imread(path);
+		if (src.empty()) continue;
+		cout << "No." << i << " is:" << endl;
+		CheckJiaban demo;
+		Mat jiaozheng = demo.jiaozheng(src);
+		Mat chuli = demo.chuli(jiaozheng);
+		vector<RotatedRect>light = demo.findLight(chuli, src);
+		RotatedRect jiaban = demo.findJiban(light, src);
+		demo.zuobiao(jiaban, src);//选择已经匹配的灯管这样准确性很高
+		imshow("result", src);
+		string path2="output_low/"+to_string(i) + ".jpg";
+		imwrite(path2, src);
+		//waitKey(0);
+	}
+
+	//Mat src = imread("src/high/8.jpg");
+	//CheckJiaban demo;
+	//Mat jiaozheng = demo.jiaozheng(src);
+	//Mat chuli= demo.chuli(jiaozheng);
+	//vector<RotatedRect>light= demo.findLight(chuli,src);
+	//RotatedRect jiaban = demo.findJiban(light,src);
+	//demo.zuobiao(jiaban,src);//选择已经匹配的灯管这样准确性很高
+	//imshow("result", src);
+	//imwrite("output/7.jpg", src);
+	//waitKey(0);
 	return 0;
 }
